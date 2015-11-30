cd4ec0b060bfbd6a10ed68bcd7a3281ab5c9a8b8
YashchukM <19Mihail@gmail.com>
2015-11-30 17:52:14 +0200

    Add seed script, modify shop_schema creation script

---
+++ seed.sql
@@ -0,0 +1,23 @@
+# Users
+INSERT INTO `shop_schema`.`user` (`id`, `fir_name`, `sec_name`, `email`, `password`, `admin`, `blacklist`)
+VALUES (0, "Vova", "Pupkin", "vovann@example.com", "asdasd", 1, 0);
+
+INSERT INTO `shop_schema`.`user` (`id`, `fir_name`, `sec_name`, `email`, `password`, `admin`, `blacklist`)
+VALUES (0, "Ivo", "Bobul", "ivo@example.com", "asdasd", 0, 0);
+
+# Products
+INSERT INTO `shop_schema`.`product` (`id`,`name`,`description`,`available`,`price`)
+VALUES (0,"Red armchair","Big and comfortable chair from Spain",1,1230.3);
+
+INSERT INTO `shop_schema`.`product` (`id`,`name`,`description`,`available`,`price`)
+VALUES (0,"Black armchair","Big and comfortable chair from Italy",1,1530.3);
+
+INSERT INTO `shop_schema`.`product` (`id`,`name`,`description`,`available`,`price`)
+VALUES (0,"Pink sofa","Family leather furniture from Brasil",1,2230.3);
+
+INSERT INTO `shop_schema`.`product` (`id`,`name`,`description`,`available`,`price`)
+VALUES (0,"Yellow sofa","Family leather furniture from Poland",1,1000.3);
+
+INSERT INTO `shop_schema`.`product` (`id`,`name`,`description`,`available`,`price`)
+VALUES (0,"Table FPX10","Small table for child room, made from wood",0,1500.0);
+
---
+++ shop_schema.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE `shop_schema` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+CREATE TABLE `shop_schema`.`product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `available` tinyint(1) NOT NULL,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `shop_schema`.`user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fir_name` varchar(30) DEFAULT NULL,
+  `sec_name` varchar(30) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  `blacklist` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `shop_schema`.`order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `paid` tinyint(1) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_user_idx` (`id_user`),
+  CONSTRAINT `fk_order_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `shop_schema`.`order_part` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_order` int(11) DEFAULT NULL,
+  `id_product` int(11) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_order_part_1_idx` (`id_order`),
+  KEY `fk_order_part_2_idx` (`id_product`),
+  CONSTRAINT `fk_order` FOREIGN KEY (`id_order`) REFERENCES `order` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_product` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
